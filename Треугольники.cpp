777dd43f7ef158622ce941d5549d3fcded5364a3
Qual0 <165184748+Qual0@users.noreply.github.com>
2024-03-28 14:52:03 +0300

    Это файл .cpp треугольников.

---
+++ Треугольники.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    
+    for (int i = 0; i < n-1; ++i) {
+        cout << "*";
+        for (int j = 0; j < i; ++j) {
+            cout << " ";
+        }
+        cout << "*\n";
+    }
+    for (int u = 0; u < n; ++u) {
+        cout << "*";
+    }
+}
